da936a4bc48ca7ee8cffea7cf557a794a7e8e932
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-02-21 13:41:35 +0100

    modify user authentication

--- server/middlewares/Auth.js
+++ server/middlewares/Auth.js
@@ -67,13 +67,35 @@ class Authentication {
    * @param {Object} req - request object
    * @param {Object} res - response object
    * @param {Function} next - run next func
-   * @return {Null} no return value
+   * @return {Null|Object} response object | no return value
    */
   static checkUserRouteAccess(req, res, next) {
-    if (req.params.id !== req.decoded.userId || req.decoded.roleId !== 1) {
-      return res.status(401).send({ status: AuthStatus.FORBIDDEN });
+    if ((String(req.decoded.userId) === req.params.id) ||
+      req.decoded.roleId === 1) {
+      next();
+    } else {
+      return res.status(403).send(AuthStatus.FORBIDDEN);
     }
-    next();
+  }
+
+  /**
+   * checkUserRouteAccess
+   * checks if the user is an admin authorising
+   * @param {Object} req - request object
+   * @param {Object} res - response object
+   * @param {Function} next - run next func
+   * @return {Null|Object} response object | no return value
+   */
+  static logout(req, res, next) {
+    const token = req.headers.authorization || req.headers['x-access-token'];
+    const decoded = req.decoded;
+
+    if (token && decoded) {
+      delete req.headers.authorization;
+      delete req.headers['x-access-token'];
+      delete req.decoded;
+      next();
+    }
   }
 }
 
