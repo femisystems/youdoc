adea553eed5f63ef7da4175873c97f1cc7f8fa4b
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-02-25 19:46:58 +0100

    modify test configuration

--- es6/test/models/role.test.js
+++ es6/test/models/role.test.js
@@ -1,35 +1,36 @@
+import { expect } from 'chai';
 import Db from '../../models/Index';
 import seed from '../../db/seeds/Role';
 
 describe('#RoleTest', () => {
   // setup
-  beforeAll(() => Db.Roles.bulkCreate(seed));
+  before(() => Db.Roles.bulkCreate(seed));
 
   // teardown
-  afterAll(() => Db.Roles.sequelize.sync({ force: true }));
+  after(() => Db.Roles.sequelize.sync({ force: true }));
 
   // create role
   describe('#Create', () => {
-    test('1. Admin role was successfully created', () => {
+    it('1. Admin role was successfully created', () => {
       Db.Roles
         .findOne({ title: 'admin' })
-        .then(role => expect(role.id).toEqual(1));
+        .then(role => expect(role.id).to.equal(1));
     });
-    test('2. Other roles(consultant, facilitator, fellow) were successfully created', () => {
+    it('2. Other roles(consultant, facilitator, fellow) were successfully created', () => {
       Db.Roles
         .findAll()
         .then((roles) => {
-          expect(roles.length).toEqual(4);
-          expect(roles[1].title).toEqual('consultant');
-          expect(roles[2].title).toEqual('facilitator');
-          expect(roles[3].title).toEqual('fellow');
+          expect(roles.length).to.equal(4);
+          expect(roles[1].title).to.equal('consultant');
+          expect(roles[2].title).to.equal('facilitator');
+          expect(roles[3].title).to.equal('fellow');
         });
     });
   });
 
   // validate create role input
   describe('#Validate', () => {
-    test('1. Should not recreate an existing role', () => {
+    it('1. Should not recreate an existing role', () => {
       const role5 = {
         id: 5,
         title: 'admin',
@@ -39,23 +40,23 @@ describe('#RoleTest', () => {
 
       Db.Roles
       .create(role5)
-      .then(role => expect(role).toBeUndefined)
+      .then(role => expect(role).to.not.exist)
       .catch((err) => {
-        expect(err.errors[0].type).toBe('unique violation');
-        expect(err.errors[0].message).toBe('title must be unique');
+        expect(err.errors[0].type).to.equal('unique violation');
+        expect(err.errors[0].message).to.equal('title must be unique');
       });
     });
-    test('2. Should not create a role with null data', () => {
+    it('2. Should not create a role with null data', () => {
       const role6 = {};
 
       Db.Roles
       .create(role6)
-      .then(role => expect(role).toBeUndefined)
+      .then(role => expect(role).to.not.exist)
       .catch((err) => {
-        expect(err.message).toBe('notNull Violation: title cannot be null');
+        expect(err.message).to.equal('notNull Violation: title cannot be null');
       });
     });
-    test('3. Should not create a role with wrong data type', () => {
+    it('3. Should not create a role with wrong data type', () => {
       const role7 = {
         id: 7,
         title: 10,
@@ -65,16 +66,16 @@ describe('#RoleTest', () => {
 
       Db.Roles
       .create(role7)
-      .then(role => expect(role).toBeUndefined)
+      .then()
       .catch((err) => {
-        expect(err.message).toBe('notNull Violation: title cannot be null');
+        expect(err.message).to.equal('notNull Violation: title cannot be null');
       });
     });
   });
 
   // update role
   describe('#Update', () => {
-    test('1. Should update role by id', () => {
+    it('1. Should update role by id', () => {
       const adminRoleId = 1;
       const update = { title: 'superAdmin' };
 
@@ -82,14 +83,14 @@ describe('#RoleTest', () => {
         .findById(adminRoleId)
         .then((role) => {
           role.update(update)
-            .then(updatedRole => expect(updatedRole.dataValues.title).toBe('superAdmin'));
+            .then(updatedRole => expect(updatedRole.dataValues.title).to.equal('superAdmin'));
         });
     });
   });
 
   // delete role
   describe('#Delete', () => {
-    test('1. Should delete role by id', () => {
+    it('1. Should delete role by id', () => {
       const consultantRoleId = 2;
 
       Db.Roles
@@ -100,7 +101,7 @@ describe('#RoleTest', () => {
               Db.Roles
                 .findById(consultantRoleId)
                 .then()
-                .catch(err => expect(err.message).toBe('relation "Roles" does not exist'));
+                .catch(err => expect(err.message).to.equal('relation "Roles" does not exist'));
             });
         });
     });
