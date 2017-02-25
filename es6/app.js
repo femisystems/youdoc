3f3c813ca58dd7e323fab1538d4514c7c1d1e013
Andela Developer <andeladeveloper@Andelas-MacBook-Pro.local>
2017-02-25 08:45:04 +0100

    seperate server port listener from main app

---
+++ es6/app.js
@@ -0,0 +1,18 @@
+// Dependencies
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import Router from './routes';
+
+// Global vars
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+// Route app
+Router(app);
+
+export default app;
--- es6/server.js
+++ es6/server.js
@@ -1,18 +1,5 @@
-// Dependencies
-import express from 'express';
-import bodyParser from 'body-parser';
-import logger from 'morgan';
-import Router from './routes';
+import app from './app';
 
-// Global vars
-const app = express();
 const port = process.env.PORT || 5001;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(logger('dev'));
-
-// Route app
-Router(app);
 
 app.listen(port);
